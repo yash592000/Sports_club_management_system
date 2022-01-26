fd413c96c2414addf949ca7c0ca1363752fb0541
yash592000 <54179071+yash592000@users.noreply.github.com>
2022-01-27 02:54:37 +0530

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,23 @@
+
+<ul id="main-menu" class="" >
+			
+    <li id="dash"><a href="index.php"><i class="entypo-gauge"></i><span>Dashboard</span></a></li>
+                
+	<li id="regis"><a href="new_entry.php"><i class="entypo-user-add"></i><span>New Registration</span></a>                
+				
+	<li id="paymnt"><a href="payments.php"><i class="entypo-star"></i><span>Payments</span></a></li>
+
+	<li class="" id="memExpand"><a href="view_mem.php"><i class="entypo-users"></i><span>Members</span></a></li>
+
+		<li class="" id="planhassubopen"><a href="#" onclick="memberExpand(2)"><i class="entypo-quote"></i><span>Sports Plan</span></a>
+
+		<ul id="planExpand">
+			<li class="active">
+				<a href="new_plan.php"><span>New Sports Plan</span></a></li>
+
+			<li><a href="view_plan.php"><span>Edit Subsciption Details</span></a></li>
+		</ul>
+
+	<li><a href="logout.php"><i class="entypo-logout"></i><span>Logout</span></a></li>
+
+</ul>	
